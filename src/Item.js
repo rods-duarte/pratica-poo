23984d7d0de8fa1bb7b2b2cb8f4b27e8cba9f130
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-09-29 20:54:53 -0300

    fix

--- src/Item.js
+++ src/Item.js
@@ -13,28 +13,28 @@ export class Item {
 }
 
 export class Beer extends Item {
-    constructor(category, description, price) {
-        super(category, description, price);
+    constructor(description, price) {
+        super("Beer", description, price);
         this.tax = 0.2; 
     }
 }
 
 export class Cigar extends Item {
-    constructor(category, description, price) {
-        super(category, description, price);
+    constructor(description, price) {
+        super("Cigar", description, price);
         this.tax = 0.25;
     }
 }
 
 export class Eletronics extends Item {
-    constructor(category, description, price) {
-        super(category, description, price);
+    constructor(description, price) {
+        super("Eletronics", description, price);
         this.tax = 0.3;
     }
 }
 
 export class Water extends Item {
-    constructor(category, description, price) {
-        super(category, description, price);
+    constructor(description, price) {
+        super("Water", description, price);
     }
 }
